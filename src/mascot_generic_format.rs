736faed5413ac2ba110674423075cf72c5d7885e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-08-02 16:15:36 +0200

    Composed builders for mascot generic format builder

--- src/mascot_generic_format.rs
+++ src/mascot_generic_format.rs
@@ -4,3 +4,40 @@ pub struct MascotGenericFormat<I, F> {
     metadata: MascotGenericFormatMetadata<I, F>,
     data: Vec<MascotGenericFormatData<F>>,
 }
+
+impl<I: Copy, F: Copy + StrictlyPositive> MascotGenericFormat<I, F> {
+    pub fn new(
+        metadata: MascotGenericFormatMetadata<I, F>,
+        data: Vec<MascotGenericFormatData<F>>,
+    ) -> Self {
+        Self {
+            metadata,
+            data,
+        }
+    }
+
+    /// Returns the feature ID of the metadata.
+    pub fn feature_id(&self) -> I {
+        self.metadata.feature_id()
+    }
+
+    /// Returns the parent ion mass of the metadata.
+    pub fn parent_ion_mass(&self) -> F {
+        self.metadata.parent_ion_mass()
+    }
+
+    /// Returns the retention time of the metadata.
+    pub fn retention_time(&self) -> F {
+        self.metadata.retention_time()
+    }
+
+    /// Returns the charge of the metadata.
+    pub fn charge(&self) -> Charge {
+        self.metadata.charge()
+    }
+
+    /// Returns the filename of the metadata.
+    pub fn filename(&self) -> Option<&str> {
+        self.metadata.filename()
+    }
+}
--- src/mascot_generic_format_builder.rs
+++ src/mascot_generic_format_builder.rs
@@ -1,26 +1,69 @@
+use std::str::FromStr;
+
 use crate::prelude::*;
 
 /// A builder for [`MascotGenericFormat`].
 pub struct MascotGenericFormatBuilder<I, F> {
-    feature_id: Option<I>,
-    parent_ion_mass: Option<F>,
-    retention_time: Option<F>,
-    charge: Option<Charge>,
-    fragmentation_spectra_level: Option<FragmentationSpectraLevel>,
-    mass_divided_by_charge_ratios: Vec<F>,
-    fragment_intensities: Vec<F>,
+    metadata_builder: MascotGenericFormatMetadataBuilder<I, F>,
+    data_builders: Vec<MascotGenericFormatDataBuilder<F>>,
 }
 
-impl<I, F> Default for MascotGenericFormatBuilder<I, F> {
-    fn default() -> Self {
+impl<I, F> MascotGenericFormatBuilder<I, F>
+where
+    I: Copy,
+    F: Copy + StrictlyPositive,
+{
+    /// Creates a new [`MascotGenericFormatBuilder`].
+    pub fn new() -> Self {
         Self {
-            feature_id: None,
-            parent_ion_mass: None,
-            retention_time: None,
-            charge: None,
-            fragmentation_spectra_level: None,
-            mass_divided_by_charge_ratios: Vec::new(),
-            fragment_intensities: Vec::new(),
+            metadata_builder: MascotGenericFormatMetadataBuilder::default(),
+            data_builders: Vec::new(),
         }
+    }
+
+    /// Builds a [`MascotGenericFormat`] from the given data.
+    pub fn build(self) -> Result<MascotGenericFormat<I, F>, String> {
+        Ok(MascotGenericFormat::new(
+            self.metadata_builder.build()?,
+            self.data_builders
+                .into_iter()
+                .map(|builder| builder.build())
+                .collect::<Result<Vec<_>, String>>()?,
+        ))
+    }
+}
+
+impl<I, F> LineParser for MascotGenericFormatBuilder<I, F>
+where
+    I: Copy + FromStr + Eq,
+    F: Copy + StrictlyPositive + FromStr + PartialEq,
+{
+    fn can_parse_line(&self, line: &str) -> bool {
+        line == "BEGIN IONS"
+            || line == "END IONS"
+            || self.metadata_builder.can_parse_line(line)
+            || self
+                .data_builders
+                .iter()
+                .any(|builder| builder.can_parse_line(line))
+    }
+
+    /// Digests the given line.
+    fn digest_line(&mut self, line: &str) -> Result<(), String> {
+        if line == "BEGIN IONS" {
+            self.data_builders
+                .push(MascotGenericFormatDataBuilder::default());
+        } else if line == "END IONS" {
+            // Do nothing.
+        } else if self.metadata_builder.can_parse_line(line) {
+            self.metadata_builder.digest_line(line)?;
+        } else {
+            self.data_builders
+                .last_mut()
+                .expect("No data builder found.")
+                .digest_line(line)?;
+        }
+
+        Ok(())
     }
 }

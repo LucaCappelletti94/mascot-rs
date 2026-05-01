83a8a02c1af6e13b78cffab27836297e970cd881
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-05-01 17:20:30 +0200

    Fuzz MGF parsing through FromStr

--- fuzz/fuzz_targets/random_documents.rs
+++ fuzz/fuzz_targets/random_documents.rs
@@ -1,16 +1,9 @@
-//! Fuzzing harness to test whether the intersection estimation works as expected.
+//! Fuzzing harness for arbitrary MGF document parsing.
 #![no_main]
 
-use arbitrary::Arbitrary;
 use mascot_rs::prelude::*;
 use libfuzzer_sys::fuzz_target;
 
-#[derive(Arbitrary, Debug)]
-struct FuzzCase {
-    document: Vec<String>,
-}
-
-
-fuzz_target!(|data: FuzzCase| {
-    let _ = MGFVec::<usize, f32>::try_from_iter(data.document.iter().map(|s| s.as_str()));
+fuzz_target!(|document: &str| {
+    let _ = document.parse::<MGFVec<usize, f32>>();
 });
